b736296e7e38b2c3b170089895bf5546f432e86d
ParthShethia25 <parthowsmultiverse@gmail.com>
2025-12-17 00:11:16 +0530

    feat: provision ec2 infrastructure using terraform

---
+++ terraform/ec2.tf
@@ -0,0 +1,21 @@
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+}
+
+resource "aws_instance" "cloud_service" {
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.cloud_service_sg.id]
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
+
+  tags = {
+    Name = "cloud-service"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  type    = string
+  default = "ap-south-1"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t3.micro"
+}
+
+variable "key_name" {
+  type = string
+}
+
+variable "allowed_ssh_cidr" {
+  type = string
+}
